f7e5444a5bfc69199e4184daef5a28cafae81e17
Ayushi Maurya <106837615+am-298@users.noreply.github.com>
2026-01-17 23:00:38 +0530

    Update tb_dff.v

--- RTL_Example/tb_dff.v
+++ RTL_Example/tb_dff.v
@@ -19,23 +19,23 @@ module tb_diff();
         .Q(Q)
     );
 
-  
+
     initial begin
         $monitor("Time %0t | reset=%b | D=%b | Q=%b", $time, reset, D, Q);
     end
 
-  
+
     initial begin
         $dumpfile("tb_diff.vcd");   // Output VCD file
-        $dumpvars(0, tb_diff); // Dump all signals in this module and instantiated modules 
-
+        $dumpvars(0, tb_diff); // Dump all signals in this module and instantiated modules
         reset = 1;
         D = 0;
 
-        #10 reset = 0;
-        #20 D = 0;
-        #5  D = 1;
-        #10 D = 0;
+        repeat(10) begin
+               @(posedge clk) ;
+                reset = $random%2;
+                D = $random % 2;
+        end
 
         #100 $finish;  // End simulation
     end
